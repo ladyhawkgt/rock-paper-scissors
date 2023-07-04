9bab9ddd1fe3bca0466fc06419a34e8b64e5be8e
ladyhawkgt <128410628+ladyhawkgt@users.noreply.github.com>
2023-07-03 21:40:10 -0700

    Change  else if to template literal

--- script.js
+++ script.js
@@ -17,15 +17,15 @@ getComputerChoice(weapons);
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "You Tie!";
+    return `No winner, ${playerSelection} ties ${computerSelection}`;
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return "You win!";
+    return `You win! ${playerSelection} beats ${computerSelection}!`;
   } else {
-    return "You lose!";
+    return `You lose! ${computerSelection} beats ${playerSelection}!`;
   }
 }
 const playerSelection = "rock";
